3ae95a9ada8e387a87e83c54a157dcc06aac2fd4
Nate Nichols <nate.nichols@utili-source.com>
2024-10-07 20:28:50 -0600

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { JsonForms } from '@jsonforms/react';
 import './App.css'
+import {
+  materialCells,
+  materialRenderers,
+} from '@jsonforms/material-renderers';
+import schema from './schema.json';
+import uischema from './uischema.json';
+import RatingControl from './RatingControl';
+import ratingControlTester from './ratingControlTester';
+import { Typography } from '@mui/material';
+
+const initialData = {
+  name: 'Send email to Adrian',
+  description: 'Confirm if you have passed the subject\nHereby ...',
+  done: true,
+  recurrence: 'Daily',
+  rating: 3,
+};
+
+const renderers = [
+  ...materialRenderers,
+  //register custom renderers
+  { tester: ratingControlTester, renderer: RatingControl },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<object>(initialData);
+  const stringifiedData = useMemo(() => JSON.stringify(data, null, 2), [data]);
+
+  const clearData = () => {
+    setData({});
+  };
 
   return (
     <>
@@ -18,16 +47,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Typography variant={'h4'}>Rendered form</Typography>
+        <JsonForms
+          schema={schema}
+          uischema={uischema}
+          data={data}
+          renderers={renderers}
+          cells={materialCells}
+          onChange={({ data }) => setData(data)}
+        />
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {data ? stringifiedData : 'Click on the button to render the form'}
       </p>
+      <button onClick={clearData}>Clear</button>
     </>
   )
 }
